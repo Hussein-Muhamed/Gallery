5dff2672da9a40149dcac1df12c46add4c60263d
Aya zain <ayaashrafzain@gmail.com>
2023-05-22 22:53:45 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LandingComponent } from './Components/landing/landing.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { AlbumsComponent } from './Components/albums/albums.component';
+import { PhotosComponent } from './Components/photos/photos.component';
+import { AddComponent } from './Components/add/add.component';
+import { EditeComponent } from './Components/edite/edite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    LandingComponent,
+    ProfileComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    AddComponent,
+    EditeComponent
   ],
   imports: [
     BrowserModule,
