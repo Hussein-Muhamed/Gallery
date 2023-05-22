becdaa4e7c6895c572306f7831a49fd7c7c1393a
asmaa abdrabo <asmaa90@outlook.com>
2023-05-23 02:27:39 +0300

    landing page

--- src/app/Servecis/users.service.ts
+++ src/app/Servecis/users.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
-  constructor() { }
+  private Base_URL = "http://localhost:3000/users";
+  constructor(private myClient:HttpClient) { }
+  GetAllUsers(){
+    return this.myClient.get(this.Base_URL);
+  }
+  GetUserByID(id:any){
+    return this.myClient.get(this.Base_URL+"/"+id);
+  }
+  AddUser(NewUser:any){
+    return this.myClient.post(this.Base_URL,NewUser);
+  }
+  UpdateUser(id:any, NewUser:any){
+    return this.myClient.put(this.Base_URL+"/"+id, NewUser);
+  }
+  DeleteUser(id:any){
+    return this.myClient.delete(this.Base_URL+"/"+id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AlbumsComponent } from './Components/albums/albums.component';
 import { PhotosComponent } from './Components/photos/photos.component';
 import { AddComponent } from './Components/add/add.component';
 import { EditeComponent } from './Components/edite/edite.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,10 @@ import { EditeComponent } from './Components/edite/edite.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
