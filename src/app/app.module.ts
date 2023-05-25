eab1eddbb048828f436692cfd4ca88f4c8f46354
Hussein <helnemar@yahoo.com>
2023-05-25 23:23:49 +0300

    add 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditeComponent } from './Components/edite/edite.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { FooterComponent } from './Components/footer/footer.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { FooterComponent } from './Components/footer/footer.component';
     AddComponent,
     EditeComponent,
     FooterComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
